219a322b905a82dba0bda7eccbddb87d4a3c6ec7
Rpotten1988 <rob@yeodesign.co.uk>
2024-06-24 12:45:23 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Woodwork4Inventor.NamingSchemeApi;
+using NamingSchemeApi;
+
+namespace DemoAddIn
+{
+    public class DemoAddIn : NamingSchemeAddIn
+    {
+        public void Init(NamingSchemeAddInContext context)
+        {
+            AddFileNamePrefix(context.NamingSchemes);
+        }
+
+        private static void AddFileNamePrefix(IEnumerable<ItemNamingScheme> namingSchemes)
+        {
+            foreach (var scheme in namingSchemes)
+            {
+                if (scheme is DocumentNamingScheme documentNamingScheme)
+                {
+                    var fileProperties = documentNamingScheme.FileProperties;
+                    var fileNameProperty = fileProperties["FileName"];
+                    fileNameProperty.NewValue = "Copy_" + fileNameProperty.Value;
+                }
+
+                AddFileNamePrefix(scheme.Children);
+            }
+        }
+
+        public string DisplayName => "Copy_ prefix add";
+        public string Description => "Add-In to add Copy_ prefix for file names";
+    }
+}
